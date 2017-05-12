ffb879b68d54745ed9c9a5b6a1efeb451eb4a3bb
IgorGSarmento <igor_gss@hotmail.com>
2017-05-11 21:39:56 -0300

    php

---
+++ php/sidebar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sidebar</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+	  
+	  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+  
+</head>
+
+<body>
+	<div class="overlay"></div>
+
+		<!-- Sidebar -->
+		<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+			<ul class="nav sidebar-nav">
+				<li class="sidebar-brand">
+					<a href="#">Panda</a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-fw fa-home"></i> Página Inicial</a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-user" aria-hidden="true"></i> Perfil</a>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-book" aria-hidden="true"></i> Disciplinas <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li class="dropdown-header">Lista de Disciplinas</li>
+							<li><a href="#">Desenvolvimento Web</a></li>
+							<li><a href="#">Sistemas Operacionais</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-power-off" aria-hidden="true"></i> Sair</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- /#sidebar-wrapper -->
+	
+
+	</body>
+</html>
