62c21f03600df20c3737d06e64e997cea4a04b8f
FelipeMSJ <felipemsoad@gmail.com>
2017-06-22 01:21:58 -0300

    login

---
+++ login/dblogin.sql
@@ -0,0 +1,19 @@
+
+--
+-- Database: `dblogin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS login (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(15) NOT NULL,
+  `user_email` varchar(40) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
